0d352c4c440ec58ea562c6641f443599b3dc9137
Matthew Cox <matthew.cox@teachforamerica.org>
2015-02-19 16:31:22 -0600

    Seeting up initial perl command test

---
+++ perl_execution.py
@@ -0,0 +1,14 @@
+import subprocess
+
+class PerlCommand(object):
+	"""Call the perl command"""
+	def __init__(self):
+		pass
+		
+	def run_crt(self):
+		subprocess.call(['perl',
+			self.path_to_crt,
+			self.cms_file,
+			self.collab_file,
+			self.user_settings_file,
+			self.output_directory])
---
+++ test/test_perl_command.py
@@ -0,0 +1,21 @@
+from unittest.mock import patch
+from ..perl_execution import PerlCommand
+
+
+def test_run_command_with_parameters():
+    pc = PerlCommand()
+    pc.cms_file = 'some_cms_file.xls'
+    pc.collab_file = 'some_collab_file.xls'
+    pc.user_settings_file = 'some_user_settings_file.txt'
+    pc.output_directory = 'some_output_directory'
+    pc.path_to_crt = 'path_to_crt'
+
+    with patch('subprocess.call') as subprocess_mock:
+        pc.run_crt()
+    expected_arguments = ['perl',
+                          'path_to_crt',
+                          'some_cms_file.xls',
+                          'some_collab_file.xls',
+                          'some_user_settings_file.txt',
+                          'some_output_directory']
+    subprocess_mock.assert_called_with(expected_arguments)
