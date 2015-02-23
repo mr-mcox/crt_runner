9bd5eea9c08000820f55f6b032bc3183620a359e
Matthew Cox <matthew.cox@teachforamerica.org>
2015-02-23 12:43:18 -0600

    Removed crt_runner because functionality has been re-written and refactored scanner

--- scanner.py
+++ scanner.py
@@ -4,11 +4,11 @@ import os.path
 class Scanner(object):
 	"""Scan folders to determine whether CRT should be run"""
 	def __init__(self):
-		pass
+		self.canary_file = 'placement_reccomendations_and_cm_level_scoring.xls'
+		self.perl_command = PerlCommand()
 
 	def scan_folder(self,folder):
-		canary_file = 'placement_reccomendations_and_cm_level_scoring.xls'
+		canary_file = self.canary_file
 		if not os.path.isfile(os.path.join(folder,canary_file)):
-			pc = PerlCommand()
-			pc.run_crt()
+			self.perl_command.run_crt()
 			
