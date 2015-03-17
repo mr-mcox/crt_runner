5cf39816bdc2ae7fc26a0774d78258c75627cc48
Matthew Cox <matthew.cox@teachforamerica.org>
2015-03-17 15:12:20 -0500

    Determine expected path for each file

--- scanner.py
+++ scanner.py
@@ -6,13 +6,14 @@ class Scanner(object):
 
     """Scan folders to determine whether CRT should be run"""
 
-    def __init__(self):
+    def __init__(self, config=None):
         """
-        
+
         .. todo:: Canary file should be replaced with more sophisticated run condtions
         """
         self.canary_file = 'placement_reccomendations_and_cm_level_scoring.xls'
         self.perl_command = PerlCommand()
+        self.config = config
 
     def scan_folder(self, folder):
         """Run CRT command if canary file missing from identified folder
@@ -23,3 +24,16 @@ class Scanner(object):
         canary_file = self.canary_file
         if not os.path.isfile(os.path.join(folder, canary_file)):
             self.perl_command.run_crt()
+
+    def path_for_file(self, institute=None, file=None):
+        config = self.config
+        base_name_map = {
+            'collab': config.collab_file_base_name,
+            'cm': config.cm_file_base_name,
+            'user_settings': config.user_settings_base_name,
+        }
+        path = "".join([
+            config.info_by_institute(institute, 'path_to_folder'),
+            config.info_by_institute(institute, 'file_prefix'),
+            base_name_map[file]])
+        return path
--- test/test_scanner.py
+++ test/test_scanner.py
@@ -1,13 +1,50 @@
 import os.path
 from ..scanner import Scanner
 from ..perl_execution import PerlCommand
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
+
 
 def test_call_runner_if_output_file_missing():
-	canary_file = 'placement_reccomendations_and_cm_level_scoring.xls'
-	with patch('os.path.isfile',return_value=False) as isfile_mock:
-		with patch.object(PerlCommand,'run_crt') as run_crt_mock:
-			s = Scanner()
-			s.scan_folder('some_folder')
-	isfile_mock.assert_called_with(os.path.join('some_folder',canary_file))
-	run_crt_mock.assert_called_with()
+    canary_file = 'placement_reccomendations_and_cm_level_scoring.xls'
+    with patch('os.path.isfile', return_value=False) as isfile_mock:
+        with patch.object(PerlCommand, 'run_crt') as run_crt_mock:
+            s = Scanner()
+            s.scan_folder('some_folder')
+    isfile_mock.assert_called_with(os.path.join('some_folder', canary_file))
+    run_crt_mock.assert_called_with()
+
+
+def test_path_for_required_file():
+
+    def return_institute_field(institute, value):
+        if value == 'path_to_folder':
+            return '/path/to/folder/'
+        if value == 'file_prefix':
+            return 'ATL'
+
+    with patch('crt_runner.config.Config') as mock:
+        config = mock.return_value
+        config.cm_file_base_name = '_CMs.xls'
+        config.collab_file_base_name = '_collabs.xls'
+        config.user_settings_base_name = '_user_settings.txt'
+        config.info_by_institute = MagicMock(
+            side_effect=return_institute_field)
+
+    expected_collab_path = "".join([
+        config.info_by_institute('ATL','path_to_folder'),
+        config.info_by_institute('ATL','file_prefix'),
+        config.collab_file_base_name])
+    expected_cm_path = "".join([
+        config.info_by_institute('ATL','path_to_folder'),
+        config.info_by_institute('ATL','file_prefix'),
+        config.cm_file_base_name])
+    expected_user_settings_path = "".join([
+        config.info_by_institute('ATL','path_to_folder'),
+        config.info_by_institute('ATL','file_prefix'),
+        config.user_settings_base_name])
+    s = Scanner(config=config)
+    assert s.path_for_file(
+        institute='Atlanta', file='collab') == expected_collab_path
+    assert s.path_for_file(institute='Atlanta', file='cm') == expected_cm_path
+    assert s.path_for_file(
+        institute='Atlanta', file='user_settings') == expected_user_settings_path
