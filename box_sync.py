50d75242041b4c7e27846b2f7dfe2a86474846e1
Matthew Cox <matthew.cox@teachforamerica.org>
2015-03-20 09:45:08 -0500

    Added tests for SyncedFile

--- box_sync.py
+++ box_sync.py
@@ -1,6 +1,7 @@
 from boxsdk import OAuth2, Client
 import os
 
+
 class BoxSync(object):
 
     """Sycronize Box and local files"""
@@ -48,3 +49,87 @@ class BoxSync(object):
             store_tokens=self.store_tokens)
         oauth.refresh(oauth.access_token)
         return Client(oauth)
+
+
+class SyncedFile(object):
+
+    """A file with both a local and Box copy"""
+
+    def __init__(self,
+                 name,
+                 box_parent_folder,
+                 local_parent_folder,
+                 parent_modify_dates=None):
+        self.name = name
+        self.box_parent_folder = box_parent_folder
+        self.local_parent_folder = local_parent_folder
+        self.parent_modify_dates = parent_modify_dates
+        box_modify_date = None
+        local_modify_date = None
+
+        # Set box and local modify dates if they exist
+        if(parent_modify_dates is not None):
+            assert type(parent_modify_dates) is dict()
+            if name in parent_modify_dates:
+                file_modify_info = parent_modify_dates[name]
+                assert 'box_modify_date' in file_modify_info
+                assert 'local_modify_date' in file_modify_info
+                box_modify_date = file_modify_info['box_modify_date']
+                local_modify_date = file_modify_info['local_modify_date']
+
+    @property
+    def modify_dates(self):
+        return None
+
+    @property
+    def box_file(self):
+        if not hasattr(self, '_box_file'):
+            self._box_file = None
+            for item in self.box_parent_folder.get_items(limit=100):
+                if item.get()['name'] == self.name:
+                    self._box_file = item
+                    break
+        return self._box_file
+
+    @box_file.setter
+    def box_file(self, value):
+        self._box_file = value
+
+    def _local_file_exists(self):
+        return os.path.isfile(self.local_file_path)
+
+    @property
+    def local_file_path(self):
+        return os.path.join(self.local_parent_folder, self.name)
+
+    def _box_file_exists(self):
+        if self.box_file is not None:
+            return True
+        else:
+            return False
+
+    def _local_file_more_recent(self):
+        if (self.box_modify_date is not None
+                and self.local_modify_date is not None):
+            if self.local_modify_date > self.box_modify_date:
+                return True
+        return False
+
+    def _box_file_more_recent(self):
+        if (self.box_modify_date is not None
+                and self.local_modify_date is not None):
+            if self.box_modify_date > self.local_modify_date:
+                return True
+        return False
+
+    def _download_box_file_to_local(self):
+        self.box_file.download_to(self.local_file_path)
+
+    def _upload_local_file_to_box_folder(self):
+        self.box_parent_folder.upload(self.local_file_path)
+
+    def _replace_box_file_with_local(self):
+        self.box_file.update_contents(self.local_file_path)
+
+    def sync_files(self):
+        pass
--- test/test_box_sync.py
+++ test/test_box_sync.py
@@ -1,7 +1,8 @@
 from unittest.mock import patch
-from ..box_sync import BoxSync
+from ..box_sync import BoxSync, SyncedFile
 import pytest
 import os
+from datetime import datetime
 
 
 @pytest.fixture
@@ -14,10 +15,52 @@ def mock_config():
 
 
 def test_store_tokens(mock_config):
-    with patch.object(BoxSync,'authenticate_client'):
+    with patch.object(BoxSync, 'authenticate_client'):
         bs = BoxSync(mock_config)
     bs.store_tokens('access', 'refresh')
     assert open(mock_config.box_access_token_file).readline() == 'access'
     assert open(mock_config.box_refresh_token_file).readline() == 'refresh'
     os.remove(mock_config.box_access_token_file)
     os.remove(mock_config.box_refresh_token_file)
+
+
+def test_file_modify_dates():
+    modify_dates = {'file': {'box_modify_date': 1420092000.0,
+                             'local_modify_date': 1420094000.0}}
+    sf = SyncedFile('file', 'box_folder', 'parent_folder', modify_dates)
+    assert sf.modify_dates == modify_dates['file']
+
+
+def test_download_box_file_when_local_does_not_exist():
+    sf = SyncedFile('file', 'box_folder', 'parent_folder')
+    sf._local_file_exists = False
+    sf._box_file_exists = True
+    with patch.object(SyncedFile,
+                      '_download_box_file_to_local') as mock_download:
+        assert mock_download.called
+
+def test_upload_local_file_when_box_copy_does_not_exist():
+    sf = SyncedFile('file', 'box_folder', 'parent_folder')
+    sf._local_file_exists = True
+    sf._box_file_exists = False
+    with patch.object(SyncedFile,
+                      '_upload_local_file_to_box_folder') as mock_upload:
+        assert mock_upload.called
+
+def test_download_box_file_when_box_more_recent():
+    sf = SyncedFile('file', 'box_folder', 'parent_folder')
+    sf._local_file_exists = True
+    sf._box_file_exists = True
+    sf._box_file_more_recent = True
+    with patch.object(SyncedFile,
+                      '_download_box_file_to_local') as mock_download:
+        assert mock_download.called
+
+def test_update_box_file_when_local_more_recent():
+    sf = SyncedFile('file', 'box_folder', 'parent_folder')
+    sf._local_file_exists = True
+    sf._box_file_exists = True
+    sf._local_file_more_recent = True
+    with patch.object(SyncedFile,
+                      '_replace_box_file_with_local') as mock_replace:
+        assert mock_replace.called
