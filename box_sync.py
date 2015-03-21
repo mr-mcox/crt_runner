af35c30e1a2e2bfc7750edf59755df33f5106d62
Matthew Cox <matthew.cox@teachforamerica.org>
2015-03-21 12:33:43 -0500

    Box sync working! Fixes #16.

--- box_sync.py
+++ box_sync.py
@@ -1,5 +1,8 @@
 from boxsdk import OAuth2, Client
+import boxsdk
 import os
+import yaml
+import dateutil.parser
 
 
 class BoxSync(object):
@@ -8,7 +11,6 @@ class BoxSync(object):
 
     def __init__(self, config):
         self.config = config
-        self.client = self.authenticate_client()
 
     def store_tokens(self, access_token, refresh_token):
         """
@@ -49,6 +51,147 @@ class BoxSync(object):
             store_tokens=self.store_tokens)
         oauth.refresh(oauth.access_token)
         return Client(oauth)
+
+    @property
+    def box_client(self):
+        if not hasattr(self, '_box_client'):
+            self._box_client = self.authenticate_client()
+        return self._box_client
+
+    @property
+    def modify_dates(self):
+        if not hasattr(self, '_modify_dates'):
+            if os.path.isfile(self.config.box_sync_modify_dates):
+                yaml_file = open(self.config.box_sync_modify_dates)
+                self._modify_dates = yaml.load(yaml_file)
+                yaml_file.close()
+            else:
+                self._modify_dates = dict()
+        return self._modify_dates
+
+    @property
+    def root_box_folder(self):
+        if not hasattr(self, '_root_box_folder'):
+            root_folder = self.box_client.folder(folder_id='0')
+            expected_name = self.config.root_box_folder_name
+            for folder in root_folder.get_items(limit=100):
+                folder_name = folder.get()['name']
+                if folder_name == expected_name:
+                    self._root_box_folder = folder
+                    break
+            else:
+                assert False, 'Root box folder "' + \
+                    expected_name + '" not found'
+        return self._root_box_folder
+
+    @property
+    def institute_folders(self):
+        if not hasattr(self, '_institute_folders'):
+            institute_folders = list()
+            for institute in self.config.institute_list:
+                sf = SyncedFolder(name=institute,
+                                  box_parent_folder=self.root_box_folder,
+                                  local_parent_folder=self.config.root_local_folder,
+                                  parent_modify_dates=self.modify_dates)
+                institute_folders.append(sf)
+            self._institute_folders = institute_folders
+        return self._institute_folders
+
+    def sync_institute_folders(self):
+        for inst_folder in self.institute_folders:
+            inst_folder.sync_folder()
+        self.refresh_modify_dates()
+
+        # Save sync records
+        sync_file = open(self.config.box_sync_modify_dates, 'w')
+        yaml.dump(self.modify_dates, sync_file)
+        sync_file.close()
+
+    def refresh_modify_dates(self):
+        for inst_folder in self.institute_folders:
+            self._modify_dates[inst_folder.name] = inst_folder.modify_dates
+
+
+class SyncedFolder(object):
+
+    """A folder with both a local and Box copy"""
+
+    def __init__(self,
+                 name,
+                 box_parent_folder,
+                 local_parent_folder,
+                 parent_modify_dates=dict()):
+        self.name = name
+        self.box_parent_folder = box_parent_folder
+        self.local_parent_folder = local_parent_folder
+        self.parent_modify_dates = parent_modify_dates
+
+    @property
+    def box_folder(self):
+        if not hasattr(self, '_box_file'):
+            self._box_folder = None
+            for item in self.box_parent_folder.get_items(limit=100):
+                if item.get()['name'] == self.name:
+                    self._box_folder = item
+                    break
+            else:
+                # Create new box folder if it hasn't been found
+                self._box_folder = self.box_parent_folder.create_subfolder(
+                    self.name)
+                self.parent_modify_dates[self.name] = dict()
+        return self._box_folder
+
+    @property
+    def local_file_path(self):
+        path = os.path.join(self.local_parent_folder, self.name)
+        if not os.path.isdir(path):
+            os.mkdir(path)
+        return path
+
+    @property
+    def files_in_folder(self):
+        if not hasattr(self, '_files_in_folder'):
+            local_path = self.local_file_path
+            local_files = [f for f in os.listdir(
+                local_path) if os.path.isfile(os.path.join(local_path, f))]
+            box_files = list()
+            for item in self.box_folder.get_items(limit=100):
+                if type(item) is boxsdk.object.file.File:
+                    box_files.append(item.get()['name'])
+            self._files_in_folder = list(set(local_files + box_files))
+        return self._files_in_folder
+
+    @property
+    def modify_dates(self):
+        if not hasattr(self, '_modify_dates'):
+            if self.name in self.parent_modify_dates:
+                self._modify_dates = self.parent_modify_dates[self.name]
+            else:
+                self._modify_dates = dict()
+        return self._modify_dates
+
+    @property
+    def synced_files(self):
+        if not hasattr(self, '_synced_files'):
+            synced_files = list()
+            for name in self.files_in_folder:
+                sf = SyncedFile(name=name,
+                                box_parent_folder=self.box_folder,
+                                local_parent_folder=self.local_file_path,
+                                parent_modify_dates=self.modify_dates)
+                synced_files.append(sf)
+            self._synced_files = synced_files
+        return self._synced_files
+
+    def refresh_modify_dates(self):
+        for sf in self.synced_files:
+            self._modify_dates[sf.name] = sf.modify_dates
+        return self.modify_dates
+
+    def sync_folder(self):
+        for sf in self.synced_files:
+            sf.sync_files()
+        self.refresh_modify_dates()
 
 
 class SyncedFile(object):
@@ -97,7 +240,7 @@ class SyncedFile(object):
                 if item.get()['name'] == self.name:
                     self._box_file = item
                     self.box_modify_date = dateutil.parser.parse(
-                        item.get()['modified_at'])
+                        item.get()['modified_at']).timestamp()
                     break
         return self._box_file
 
@@ -111,7 +254,10 @@ class SyncedFile(object):
 
     @property
     def local_file_path(self):
-        return os.path.join(self.local_parent_folder, self.name)
+        path = os.path.join(self.local_parent_folder, self.name)
+        if os.path.isfile(path):
+            self.local_modify_date = os.path.getmtime(path)
+        return path
 
     @property
     def _box_file_exists(self):
@@ -137,7 +283,9 @@ class SyncedFile(object):
         return False
 
     def _download_box_file_to_local(self):
-        self.box_file.download_to(self.local_file_path)
+        file_to_write = open(self.local_file_path, 'wb')
+        self.box_file.download_to(file_to_write)
+        file_to_write.close()
         self.local_modify_date = self.box_modify_date
 
     def _upload_local_file_to_box_folder(self):
